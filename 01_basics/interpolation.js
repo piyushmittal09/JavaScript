f9ac93be4353aa65540ec9921f473de2c9df8a0b
Piyush Mittal <128731943+piyushmittal09@users.noreply.github.com>
2024-08-15 17:54:35 +0000

    updated

---
+++ 01_basics/interpolation.js
@@ -0,0 +1,6 @@
+let name = Piyush
+let repoCount = 2
+
+// console.log(name + counting + " Value"); //
+
+console.log("hi");
--- 01_basics/string-interpolation.js
+++
@@ -1,6 +0,0 @@
-const name = Piyush
-const repoCount = 2
-
-// console.log(name + counting + " Value");
-
-console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
