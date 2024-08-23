345ab3083c0e7a06e01ec35e19d4074b55b935a5
Piyush Mittal <128731943+piyushmittal09@users.noreply.github.com>
2024-08-23 04:01:27 +0000

    updated

---
+++ 01_basics/Strings.js
@@ -0,0 +1,7 @@
+// let str1 = "This is a string primitive";
+// let str2 = 'This is also a string primitive';
+// let str3 = `Yet another string primitve`;
+
+// const string4 = new String("A String object");
+
+// console.log(str1, str2, str3, string4);
