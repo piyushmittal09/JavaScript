ea1ffacf6760927e013ecdca8089a2fe826156fa
Piyush Mittal <128731943+piyushmittal09@users.noreply.github.com>
2024-08-24 17:55:35 +0000

    updated

--- 01_basics/Strings.js
+++ 01_basics/Strings.js
@@ -5,3 +5,24 @@
 // const string4 = new String("A String object");
 
 // console.log(str1, str2, str3, string4);
+
+// const gameName = new String("piyush mittal");
+
+// console.log(gameName.toUpperCase());
+
+// const newName = gameName.toUpperCase();
+// console.log(newName);
+
+// const trimmer = "        Asla tera yaar bhi rakhe      ";
+// console.log(trimmer.trim());
+
+// const stringObj = new String("this is string object");
+// console.log(stringObj);
+
+// const valueString = stringObj.valueOf();
+// console.log(valueString);
+
+// const emailId = "piyushmittal1983@gmail.com";
+// const newEmailId = emailId.substring(0, 16);
+// console.log(newEmailId);
+
