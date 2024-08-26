65fcda676887698a31bc7c7caa1ee7e8ff1d21e2
Piyush Mittal <128731943+piyushmittal09@users.noreply.github.com>
2024-08-26 14:59:46 +0000

    updated

---
+++ 01_basics/numbers.js
@@ -0,0 +1,17 @@
+// const hitpoints = 100;
+// console.log(hitpoints);
+
+
+// const balance = new Number(200);
+// console.log(balance);
+
+// console.log(typeof balance.toString());
+
+// console.log(balance.toFixed(2))
+
+// const someNumber = 482.5543
+
+// console.log(someNumber.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
