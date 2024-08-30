ecf44237252cf6aa3065db3ee3b7dd397db32df8
Piyush Mittal <128731943+piyushmittal09@users.noreply.github.com>
2024-08-30 06:21:43 +0000

    Everything-UpDated

--- 01_basics/dates.js
+++ 01_basics/dates.js
@@ -14,3 +14,15 @@
 
  let myCreatedDate = new Date(2006, 4, 12, 12, 0); // my birthday!
 console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate2 = new Date("2006-05-12"); // my birthday!
+console.log(myCreatedDate2.toLocaleString());
+
+let myCreatedDate3 = new Date("05-12-2006"); // my birthday!
+console.log(myCreatedDate3.toLocaleString());
+
+let timeStamp = Date.now();
+
+console.log(timeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
